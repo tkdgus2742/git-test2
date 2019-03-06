a8d87cb4333b84b16f84c96e17f1bfcedd360595
D7703_11 <d7703_11@d7703-11ui-imac.local>
2019-03-06 16:46:22 +0900

    Initial Commit

---
+++ git test2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git test2
+//
+//  Created by D7703_11 on 2019. 3. 6..
+//  Copyright © 2019년 dit.ac.kr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
